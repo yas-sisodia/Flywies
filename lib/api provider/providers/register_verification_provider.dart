073d5bd733a8953356b3c711bf5b3d95fc47bdd3
Yash Sisodia <yashsisodia701@gmail.com>
2023-08-25 03:59:47 +0530

    Sign Up working now

--- lib/api provider/providers/register_verification_provider.dart
+++ lib/api provider/providers/register_verification_provider.dart
@@ -1,28 +1,26 @@
 import 'package:dio/dio.dart';
+import 'package:raghu_education/global_variables.dart';
 import 'package:raghu_education/view/signUp/profile/profile_screen.dart';
 import 'package:raghu_education/widgets/commonLoader.dart';
 import 'package:get/get.dart' as g;
-class RegisterVerificationEndPoint{
+
+class RegisterVerificationEndPoint {
   Dio client;
   RegisterVerificationEndPoint({required this.client});
 
-
-  registerVerifyOtp(var email, var otp) async{
+  registerVerifyOtp(var email, var otp) async {
     try {
       CommanLoader.loader();
-      var data = {
-            "otp": otp,
-    "userIdentifier": email
-      };
-      Response r = await client.post("user/signup-otp", data: data);
+      var data = {"email": email, "otp": otp};
+      Response r = await client.post("${GlobalVariables.baseUrl}/verify-otp",
+          data: data);
       print(r.statusCode);
       print(r.data);
-       CommanLoader.hideLoader();
-      if (r.statusMessage == 'OK') {
-          g.Get.to(()=> ProfileScreen());
-      } else {
-      }
-    }on DioError catch (e) {
+      CommanLoader.hideLoader();
+      if (r.statusCode == 200) {
+        g.Get.to(() => ProfileScreen());
+      } else {}
+    } on DioError catch (e) {
       CommanLoader.hideLoader();
       CommanLoader.message(e.response!.data['msg']);
     }
--- lib/api provider/providers/registor_endpoint_provider.dart
+++ lib/api provider/providers/registor_endpoint_provider.dart
@@ -1,34 +1,42 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
 import 'package:get/get.dart' as g;
 import 'package:raghu_education/view/signUp/verify/verify.dart';
 import 'package:raghu_education/widgets/commonLoader.dart';
 
-class RegisterEndPoint{
+class RegisterEndPoint {
   Dio client;
   RegisterEndPoint({required this.client});
 
-  RegisterApi(var emailOrMobile , var password, var confirm) async{
+  RegisterApi(var emailOrMobile, var password, var confirm) async {
     try {
       CommanLoader.loader();
-      var data = {
-        "userIdentifier": emailOrMobile,
-    "password": password,
-    "confirmPassword": confirm
-      };
+      var data = json.encode({
+        "email": emailOrMobile,
+        // "mobileNumber": "8449193291",
+        "password": password,
+        "confirmPassword": confirm
+      });
 
-      Response r = await client.post('user/signup', data: data);
+      Response r =
+          await client.post('http://13.233.114.64:8000/signup', data: data);
       print(r.statusCode);
       print(r.data);
       CommanLoader.hideLoader();
-      if (r.statusMessage == 'OK') {
-      g.Get.to(()=> VerifyScreen());
+      // if (r.statusMessage == "Signup successful") {
+      if (r.statusCode == 201) {
+        print("Inside status code");
+        // print(r.);
+        // g.Get.to(() => VerifyScreen());
+        g.Get.offAll(() => VerifyScreen());
       } else {
-      CommanLoader.message(r.data['msg']);
+        print("Inside else");
+        CommanLoader.message(r.data['msg']);
       }
     } on DioError catch (err) {
       CommanLoader.hideLoader();
       CommanLoader.message(err.response!.data['msg']);
     }
-
   }
 }
